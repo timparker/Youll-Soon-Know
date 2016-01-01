bcfd581dba8c2f232b57c13b0b3867979a28d3c5
Tim Parker <tim@mixcloud.com>
2016-01-01 18:54:49 +0000

    date format

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,12 +4,16 @@ export default Ember.Route.extend({
   model: function () {
     return [
       {
-        title: "Guest Name",
-        date: "2016-1-13 3pm"
+        title: "Guest TBA",
+        date: "2016-1-13",
+        start: "15:00",
+        end: "17:00"
       },
       {
-        title: "Guest Name",
-        date: "2016-1-27 3pm"
+        title: "Guest TBA",
+        date: "2016-1-27",
+        start: "15:00",
+        end: "17:00"
       },
     ]
   }
