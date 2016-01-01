909902f22bfe3613a0d0795316048c5e62b039a6
Tim Parker <tim@mixcloud.com>
2016-01-01 16:32:46 +0000

    JSON Archives for 2015

--- app/routes/shows.js
+++ app/routes/shows.js
@@ -8,13 +8,226 @@ export default Ember.Route.extend({
         title: "Raminski",
         created: "2015-12-16",
         play: "/youllsoonknow/nts-16122015-w-special-raminski",
+        text: "",
+        download: "http://bit.ly/1MjsNE2",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-16th-december-2015",
         image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/db2985cd-84fc-4b33-be81-fd5a00b646cb.png"
       },
       {
         title: "Fracture",
         created: "2015-12-02",
         play: "/youllsoonknow/nts-2122015-w-special-guest-fracture",
+        text: "",
+        download: "http://bit.ly/1To627o",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-fracture-2nd-december-2015",
         image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/aa5647ee-a187-4858-9c95-516cc2c56f1d.png"
+      },
+      {
+        title: "Pixelord",
+        created: "2015-18-11",
+        play: "/youllsoonknow/nts-18112015-w-special-guest-pixelord",
+        text: "",
+        download: "http://bit.ly/1HZK0Hf",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-18th-november-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/286805a0-6c81-4de8-b44c-c99e56fecb3a.jpg"
+      },
+      {
+        title: "Mystery Cave",
+        created: "2015-4-11",
+        play: "/youllsoonknow/nts-4112015-w-special-guest-mystery-cave",
+        text: "",
+        download: "http://bit.ly/1NSFhGF",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-4th-november-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/8350fadd-978c-4342-a282-24c6bace45af.jpg"
+      },
+      {
+        title: "Kidkanevil & DJ Nonames",
+        created: "2015-21-10",
+        play: "/youllsoonknow/nts-20102015-w-special-guests-kidkanevil-nonames",
+        text: "",
+        download: "http://bit.ly/1MV6ZnH",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-21st-october-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/66e00ee2-e03b-4b1f-8048-71966eed1dc0.jpg"
+      },
+      {
+        title: "Iglooghost",
+        created: "2015-7-10",
+        play: "/youllsoonknow/nts-7102015-w-special-guest-iglooghost",
+        text: "",
+        download: "http://bit.ly/1R1c4tA",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-iglooghost-7th-october-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/e3bd22b0-2bf7-4c6f-b42a-87dcc0b237c0.png"
+      },
+      {
+        title: "Ivylab, Deft & SP:MC",
+        created: "2015-23-9",
+        play: "/youllsoonknow/nts-2392015-w-special-guests-ivylab-deft-spmc",
+        text: "",
+        download: "http://bit.ly/1R45rqW",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-23rd-september-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/a91ca477-1586-4214-a412-93762fc36c36.jpg"
+      },
+      {
+        title: "ItsNate & JD Reid",
+        created: "2015-9-9",
+        play: "/youllsoonknow/nts-992015-w-special-guests-itsnate-jd-reid",
+        text: "",
+        download: "http://bit.ly/1XQ27Ez",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-its-nate-jd-reid-9th-september-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/0a81674a-a748-466a-9379-6bdd2e0a0da6.png"
+      },
+      {
+        title: "Tk Kayembe & Defcon",
+        created: "2015-2-9",
+        play: "/youllsoonknow/nts-292015-w-special-guests-tk-kayembe-defcon",
+        text: "",
+        download: "http://bit.ly/1JNJhmY",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tk-does-werk-defcon-2nd-august-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/34f91da8-bb8c-4e3b-bead-af029d181993.jpg"
+      },
+      {
+        title: "Lewis James & My Panda Shall Fly",
+        created: "2015-12-8",
+        play: "/youllsoonknow/nts-1282015-w-special-guests-lewis-james-my-panda-shall-fly",
+        text: "",
+        download: "http://bit.ly/1f9PtNV",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-my-panda-shall-fly-lewis-james-12th-august-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/d6c58368-7647-4551-abbf-6ee2ce619708.png"
+      },
+      {
+        title: "SP:MC",
+        created: "2015-29-7",
+        play: "/youllsoonknow/nts-2972015-w-special-guest-spmc",
+        text: "",
+        download: "http://bit.ly/1fLEb2Y",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-29th-july-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/dfeff5b1-5ddb-4744-87db-355ef598e45f.jpg"
+      },
+      {
+        title: "Parris & Etch",
+        created: "2015-15-7",
+        play: "/youllsoonknow/nts-1572015-w-special-guests-parris-etch",
+        text: "",
+        download: "http://bit.ly/1McJXZc",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-etch-parris-15th-july-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/cb966411-6326-4390-b497-ee82bb162373.jpg"
+      },
+      {
+        title: "Stray",
+        created: "2015-1-7",
+        play: "/youllsoonknow/nts-162015-w-special-guest-stray",
+        text: "",
+        download: "http://bit.ly/1GRwQVS",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-1st-june-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/88f9c535-e927-4a85-80fa-2d5e6178504f.jpg"
+      },
+      {
+        title: "Live from Dalston Roofpark",
+        created: "2015-3-6",
+        play: "/youllsoonknow/nts-362015-live-from-dalston-roofpark",
+        text: "",
+        download: "http://bit.ly/1djwHTD",
+        nts: "",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/e11c1913-2608-4268-8710-f75a5464e43e.png"
+      },
+      {
+        title: "Submerse",
+        created: "2015-20-5",
+        play: "/youllsoonknow/nts-2052015-w-special-guest-submerse",
+        text: "",
+        download: "http://bit.ly/1JE7QXy",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-20th-may-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/5abe82c2-c0d5-440a-8691-8268bc26d557.png"
+      },
+      {
+        title: "Beat Soup",
+        created: "2015-6-5",
+        play: "/youllsoonknow/nts-652015-w-special-guests-beat-soup-nutriot",
+        text: "",
+        download: "http://bit.ly/1zOrmhV",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-6th-may-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/434bc40e-c09d-498a-9d0f-6c3731683462.png"
+      },
+      {
+        title: "S.Maharba Special",
+        created: "2015-22-4",
+        play: "/youllsoonknow/nts-2242015-smaharba-special",
+        text: "https://soundcloud.com/timparker/nts-2242015-smaharba-special",
+        download: "http://bit.ly/1aVCjSK",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-6th-may-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/f0e2a67a-08b0-4f52-97a0-c8f682a046e9.jpg"
+      },
+      {
+        title: "Deco & Origin live from LA",
+        created: "2015-8-4",
+        play: "/youllsoonknow/nts-842015-w-special-guests-deco-origin-live-from-la",
+        text: "",
+        download: "http://bit.ly/1am2Jgo",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-deco-origin-live-from-la-8th-april-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/4258a344-e307-42e8-9a00-15a7a5061cb2.png"
+      },
+      {
+        title: "Fine Grains",
+        created: "2015-25-3",
+        play: "/youllsoonknow/nts-2532015-w-fine-grains",
+        text: "",
+        download: "http://bit.ly/1BQgDgG",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-fine-grains-records-25th-march-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/e60410da-8921-4fb8-81eb-971388b4df15.jpg"
+      },
+      {
+        title: "Mount Bank & Wriggly Scott",
+        created: "2015-11-3",
+        play: "/youllsoonknow/nts-1132015-w-special-guests-mount-bank-wriggly-scott",
+        text: "",
+        download: "http://bit.ly/1Dex50P",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-11th-march-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/1239f4f3-c724-4e29-be1c-1cb465002ace.jpg"
+      },
+      {
+        title: "The Purist & Grinel",
+        created: "2015-25-2",
+        play: "/youllsoonknow/nts-2522015-w-special-guests-the-purist-grinel",
+        text: "",
+        download: "http://bit.ly/1APwhf7",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-25th-february-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/de3b882c-71d6-45a6-9bd7-112f5ee9db48.jpg"
+      },
+      {
+        title: "Bambooman & Aurora Dee Raynes",
+        created: "2015-11-2",
+        play: "/youllsoonknow/nts-1122015-w-special-guests-bambooman-aurora-dee-raynes",
+        text: "",
+        download: "http://bit.ly/1Dmepuj",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-11th-february-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/2a2029c6-0771-4493-b1ab-3d5de0d7db2a.jpg"
+      },
+      {
+        title: "Koenraad",
+        created: "2015-21-1",
+        play: "/youllsoonknow/nts-2822015-w-special-guest-koenraad",
+        text: "",
+        download: "http://bit.ly/1HDi6ju",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-28th-january-2015",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/8034a4a2-aa3e-47b4-8042-9b604dd30e99.png"
+      },
+      {
+        title: "Tehbis & Touchy Subject",
+        created: "2015-7-1",
+        play: "/youllsoonknow/nts-712015-w-special-guests-tehbis-touchy-subject",
+        text: "",
+        download: "http://bit.ly/1Dknrpz",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-tim-parker-tehbis-touchy-subject-7th-january-2014",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/12644b6b-dd57-4c66-b7f3-25f400178e73.jpg"
+      },
+      {
+        title: "J Todd & Mr E",
+        created: "2015-1-1",
+        play: "/youllsoonknow/nts-112015-w-special-guests-j-todd-mr-e",
+        text: "",
+        download: "http://bit.ly/16fb2Jx",
+        nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-31st-december-2014",
+        image: "https://thumbnail.mixcloud.com/w/640/h/640/q/100/upload/images/extaudio/a54aca77-543e-4c9e-9f94-5666a672da49.jpg"
       }
     ]
   }
