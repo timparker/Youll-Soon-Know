056b49d2e24a340e96c99faf069f8eda27ea7396
Tim Parker <tim@mixcloud.com>
2016-01-03 16:53:38 +0000

    better schedule

--- app/controllers/schedule.js
+++ app/controllers/schedule.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   latestSchedule: function () {
-    return this.get("model").slice(0, 5);
+    return this.get("model").slice(0, 10);
   }.property("model"),
+
 });
--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function () {
     return [
       {
-        title: "Guest TBA",
+        title: "Guest ELWD",
         date: "2016-1-13",
         start: "15:00",
         end: "17:00"
@@ -12,6 +12,24 @@ export default Ember.Route.extend({
       {
         title: "Guest TBA",
         date: "2016-1-27",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "Guest TBA",
+        date: "2016-2-10",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "Guest TBA",
+        date: "2016-3-9",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "Guest TBA",
+        date: "2016-3-23",
         start: "15:00",
         end: "17:00"
       },
