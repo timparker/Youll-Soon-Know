f1296198e31a4b75df9bddfe4896ff70fc037915
Tim Parker <tim@mixcloud.com>
2016-01-25 18:09:14 +0000

    animate player in when show is selected

--- app/components/mixcloud-player.js
+++ app/components/mixcloud-player.js
@@ -4,18 +4,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: "mixcloud-player",
   classNameBindings: ["isPlaying", "hasSong"],
-  
+
   url: null,
   hasSetup: false,
   isPlaying: true,
   hasSong: Ember.computed.notEmpty("show"),
-  
+
   setup: function () {
     let iframe = document.getElementById("mixcloud-iframe");
     iframe.onload = function () {
       let widget = Mixcloud.PlayerWidget(iframe);
       this.set("widget", widget);
-      
+
       widget.ready.then(() => {
         this.set("hasSetup", true);
         widget.events.buffering.on(() => this.onBuffering());
@@ -23,41 +23,41 @@ export default Ember.Component.extend({
         widget.events.pause.on(() => this.onPause());
         widget.events.progress.on((ev) => this.onProgress(ev));
         widget.events.ended.on((ev) => this.onEnded());
-      })    
+      })
 
     }.bind(this);
   }.on("didInsertElement"),
-  
+
   changeSong: function () {
     this.set("isPlaying", true);
-    
+
     if (this.get("hasSetup") && this.get("show")) {
       let widget = this.get("widget");
       widget.load(this.get("show.play"), this.get("isPlaying"))
     }
   }.observes("hasSetup", "show", "show.url"),
-  
+
   onBuffering: function () {
     this.set("buffering", true);
   },
-  
+
   onPlay: function () {
     this.set("buffering", false);
     this.set("isPlaying", true);
   },
-  
+
   onPause: function () {
     this.set("isPlaying", false);
   },
-  
+
   onProgress: function (ev) {
     console.log(ev);
   },
-  
+
   onEnded: function () {
     this.set("song", null);
   },
-  
+
   actions: {
     play: function () {
       this.get("widget").play();
