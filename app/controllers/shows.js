4aac321aad4948493a77a5f8c71e6c12aeb73581
Rik Lomas <lomas.rik@gmail.com>
2016-01-25 13:08:55 +0000

    reordering shows

--- app/controllers/shows.js
+++ app/controllers/shows.js
@@ -1,25 +1,4 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-
-  latestShows: function () {
-    return this.get("model").slice(0, 2);
-  }.property("model"),
-
-  byYears: function () {
-    // turn into a {} like
-    // { 2015: [], 2014: [{}, {}, {}] }
-    return this.get("model").reduce((prev, curr) => {
-      let year = curr.created.split("-")[0];
-
-      if (prev[year] === undefined) {
-        prev[year] = [curr];
-      } else {
-        prev[year].push(curr);
-      }
-
-      return prev;
-    }, {});
-  }.property("model")
-  
+export default Ember.Controller.extend({  
 });
