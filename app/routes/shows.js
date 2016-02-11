18fe92b68c951d5b1098d1a0cb94f53d9fb45f30
Tim Parker <tim@mixcloud.com>
2016-02-11 12:37:19 +0000

    Thelem

--- app/routes/shows.js
+++ app/routes/shows.js
@@ -4,6 +4,14 @@ export default Ember.Route.extend({
   model: function () {
     return [
         {
+          title: "Thelem",
+          created: "2016-02-10",
+          play: "/youllsoonknow/nts-2712016-w-special-guest-joe-shakespeare",
+          text: "Joined for this show by Joe Shakespeare of Knives & Ecology Tapes. Lots of exclusives in this one.",
+          download: "https://drive.google.com/file/d/0B0IPzmV4gS38bHFUMHFDMEpXSFU/view?usp=sharing",
+          nts: "http://www.nts.live/shows/youll-soon-know/episodes/youll-soon-know-w-joe-shakespeare-27th-january-2016"
+        },
+        {
           title: "Joe Shakespeare",
           created: "2016-01-27",
           play: "/youllsoonknow/nts-2712016-w-special-guest-joe-shakespeare",
