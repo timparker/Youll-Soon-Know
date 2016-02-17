6bc68a52651b8ee2bae2af176bbb8bd486c4fd2b
Tim Parker <tim@iamtimparker.com>
2016-02-17 10:28:26 +0000

    Update schedule.js

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -34,7 +34,7 @@ export default Ember.Route.extend({
         end: "17:00"
       },
       {
-        title: "TBA",
+        title: "Streets of Beige",
         date: "2016-04-20",
         start: "15:00",
         end: "17:00"
