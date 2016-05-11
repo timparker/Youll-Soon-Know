11261c5221728ed974aa33cf2b1f83a02b6dc080
Tim Parker <tim@iamtimparker.com>
2016-05-11 14:06:20 +0100

    Update schedule.js

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,12 +4,6 @@ export default Ember.Route.extend({
   model: function () {
     return [
       {
-        title: "Know V.A",
-        date: "2016-05-04",
-        start: "15:00",
-        end: "17:00"
-      },
-      {
         title: "Moxie",
         date: "2016-05-18",
         start: "15:00",
