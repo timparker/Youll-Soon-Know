baa4fa173d29bd4e4956afea84c3ba0bc137630b
Tim Parker <tim@mixcloud.com>
2016-07-01 00:03:16 +0100

    Fix

--- app/routes/shows.js
+++ app/routes/shows.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return []
+    return [
         {
           title: "Sonnyjim & Yukka",
           created: "2016-06-29",
