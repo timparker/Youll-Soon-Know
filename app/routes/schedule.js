9e99f2f9b9a156f3224d001194e6ef925c1b1863
Tim Parker <tim@iamtimparker.com>
2016-08-31 11:20:16 +0100

    Update schedule.js

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function () {
     return [
       {
-        title: "Al Nather",
+        title: "Al Nather & Itoa",
         date: "2016-09-07",
         start: "15:00",
         end: "17:00"
@@ -14,6 +14,24 @@ export default Ember.Route.extend({
         date: "2016-09-21",
         start: "15:00",
         end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2016-10-05",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2016-10-19",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA [On NTS INT]",
+        date: "2016-11-02",
+        start: "12:00",
+        end: "14:00"
       },
     ]
   }
