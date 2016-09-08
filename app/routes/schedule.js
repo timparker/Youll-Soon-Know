ace3d61cf8c905b89b9d9b1c4e6af39d428a71dd
Tim Parker <tim@mixcloud.com>
2016-09-08 11:52:12 +0100

    7th sep show

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,12 +4,6 @@ export default Ember.Route.extend({
   model: function () {
     return [
       {
-        title: "Al Nather & Itoa",
-        date: "2016-09-07",
-        start: "15:00",
-        end: "17:00"
-      },
-      {
         title: "Live from Japan",
         date: "2016-09-21",
         start: "15:00",
