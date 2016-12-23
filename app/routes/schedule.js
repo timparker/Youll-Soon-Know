fe4788fea9794028f211bc28da4babb0f818635c
Tim Parker <tim@mixcloud.com>
2016-12-23 11:25:20 +0000

    schedule

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,12 +4,6 @@ export default Ember.Route.extend({
   model: function () {
     return [
       {
-        title: "Etch",
-        date: "2016-12-28",
-        start: "15:00",
-        end: "17:00"
-      },
-      {
         title: "Morriarchi",
         date: "2016-01-11",
         start: "15:00",
@@ -18,6 +12,30 @@ export default Ember.Route.extend({
       {
         title: "Photay",
         date: "2016-01-25",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2016-02-08",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2016-02-22",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2016-03-08",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2016-03-22",
         start: "15:00",
         end: "17:00"
       },
