8173f00980ae036150d463bd42edcc909bc86f40
Tim Parker <tim@iamtimparker.com>
2017-03-14 13:37:35 +0000

    Update schedule.js

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function () {
     return [
       {
-        title: "Fracture",
+        title: "Fracture & SP:MC",
         date: "2017-03-22",
         start: "15:00",
         end: "17:00"
