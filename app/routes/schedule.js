64ccf59947618ed9abc502d4e3cd1f675217724e
Tim Parker <tim@iamtimparker.com>
2017-05-26 12:23:26 +0100

    Update schedule.js

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -10,7 +10,7 @@ export default Ember.Route.extend({
         end: "17:00"
       },
       {
-        title: "Dolenz",
+        title: "The NorthaZe & Dolenz",
         date: "2017-06-14",
         start: "15:00",
         end: "17:00"
