9e72b0d7ea2a6abf93fe04c79bb7ee5534df8054
Tim Parker <tim@mixcloud.com>
2017-06-19 09:36:27 +0100

    Schedule update

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function () {
     return [
       {
-        title: "TBA",
+        title: "Kidkanevil",
         date: "2017-06-28",
         start: "15:00",
         end: "17:00"
@@ -18,6 +18,36 @@ export default Ember.Route.extend({
       {
         title: "TBA",
         date: "2017-07-26",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2017-08-23",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2017-09-20",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2017-10-18",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2017-11-15",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2017-12-13",
         start: "15:00",
         end: "17:00"
       }
