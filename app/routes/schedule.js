2da472b07b50f4f7be07adbad74c94471761a139
Tim Parker <tim@iamtimparker.com>
2017-10-31 22:08:13 +0000

    Update schedule.js

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,13 +4,13 @@ export default Ember.Route.extend({
   model: function () {
     return [
       {
-        title: "Wylie Cable",
+        title: "Ivy Lab & Wylie Cable",
         date: "2017-11-15",
         start: "15:00",
         end: "17:00"
       },
       {
-        title: "Repeat Pattern + Stupid Kozo",
+        title: "Repeat Pattern & Stupid Kozo",
         date: "2017-12-13",
         start: "15:00",
         end: "17:00"
