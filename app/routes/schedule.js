600fc3382fe9f87e3badd1030aafa26ceb389f10
Tim Parker <tim@iamtimparker.com>
2017-12-12 16:49:19 +0000

    Update schedule.js

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -32,6 +32,60 @@ export default Ember.Route.extend({
         date: "2018-04-04",
         start: "15:00",
         end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2018-05-02",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2018-05-30",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2018-06-27",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2018-07-25",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2018-08-22",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2018-09-19",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2018-10-17",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2018-11-14",
+        start: "15:00",
+        end: "17:00"
+      },
+      {
+        title: "TBA",
+        date: "2018-12-12",
+        start: "15:00",
+        end: "17:00"
       }
     ];
   }
