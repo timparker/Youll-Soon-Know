ee5c2493f3718d5fadf2a8c8d9c44dee692e67d6
Tim Parker <tim@iamtimparker.com>
2018-04-06 17:29:21 +0100

    Update schedule.js

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function() {
     return [
       {
-        title: "TBA",
+        title: "Borka",
         date: "2018-05-02",
         start: "15:00",
         end: "17:00"
