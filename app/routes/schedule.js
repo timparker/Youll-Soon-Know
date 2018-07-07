9fccc0de3c6299f7250857a11616b79486a2754c
Tim Parker <tim@mixcloud.com>
2018-07-07 23:47:24 +0100

    Update schedule.js

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function() {
     return [
       {
-        title: "TBA",
+        title: "Crssspace",
         date: "2018-07-25",
         start: "15:00",
         end: "17:00"
