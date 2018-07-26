735db56b955ac07900fba0790aba0d13ce24d943
Tim Parker <tim@mixcloud.com>
2018-07-26 12:21:55 +0100

    updated

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,12 +4,6 @@ export default Ember.Route.extend({
   model: function() {
     return [
       {
-        title: "Crssspace",
-        date: "2018-07-25",
-        start: "15:00",
-        end: "17:00"
-      },
-      {
         title: "Cunfa, Russ Ryan & Stephen Valentini",
         date: "2018-08-23",
         start: "14:00",
--- app/routes/shows.js
+++ app/routes/shows.js
@@ -4,6 +4,15 @@ export default Ember.Route.extend({
   model: function() {
     return [
       {
+        title: "Crssspace",
+        created: "2018-07-25",
+        play: "/NTSRadio/youll-soon-know-25th-july-2018/",
+        text: "",
+        download: "",
+        nts:
+          "https://www.nts.live/shows/youll-soon-know/episodes/you-ll-soon-know-w-tim-parker-25th-july-2018"
+      },
+      {
         title: "Title & Sonnyjim",
         created: "2018-06-27",
         play: "/NTSRadio/youll-soon-know-27th-june-2018/",
