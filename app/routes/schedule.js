9d076f6a6ddd939af6267ea9f21de315707694fa
Tim Parker <timparker@tim-mbp.home>
2019-03-10 21:51:55 +0000

    Show Dates

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -20,6 +20,48 @@ export default Ember.Route.extend({
       date: "2019-05-30",
       start: "14:00",
       end: "16:00"
+    },
+    {
+      title: "TBA",
+      date: "2019-06-27",
+      start: "14:00",
+      end: "16:00"
+    },
+    {
+      title: "TBA",
+      date: "2019-07-25",
+      start: "14:00",
+      end: "16:00"
+    },
+    {
+      title: "TBA",
+      date: "2019-08-22",
+      start: "14:00",
+      end: "16:00"
+    },
+    {
+      title: "TBA",
+      date: "2019-09-19",
+      start: "14:00",
+      end: "16:00"
+    },
+    {
+      title: "TBA",
+      date: "2019-10-17",
+      start: "14:00",
+      end: "16:00"
+    },
+    {
+      title: "TBA",
+      date: "2019-11-14",
+      start: "14:00",
+      end: "16:00"
+    },
+    {
+      title: "TBA",
+      date: "2019-12-12",
+      start: "14:00",
+      end: "16:00"
     }
     ];
   }
