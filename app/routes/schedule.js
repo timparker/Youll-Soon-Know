46a75d785c5e8b92a641b02a19dd895ab51bcf0a
Tim Parker <timparker@tim-mbp.home>
2019-10-10 14:05:13 +0100

    schedule

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -10,7 +10,7 @@ export default Ember.Route.extend({
         end: "16:00"
       },
       {
-        title: "TBA",
+        title: "Iamnobodi",
         date: "2019-11-14",
         start: "14:00",
         end: "16:00"
