023410cf0431b0b0d4d41a07938af61360fbaaaf
Tim Parker <timparker@Tim-MBP.local>
2019-11-11 15:07:24 +0000

    guests

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,13 +4,13 @@ export default Ember.Route.extend({
   model: function() {
     return [
       {
-        title: "IAMNOBODI",
+        title: "IAMNOBODI & Verbz",
         date: "2019-11-14",
         start: "14:00",
         end: "16:00"
       },
       {
-        title: "TBA",
+        title: "NAbstract",
         date: "2019-12-12",
         start: "14:00",
         end: "16:00"
