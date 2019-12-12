20493dde284198db0aaedc84f736cee907634eac
Tim Parker <timparker@tim-mbp.home>
2019-12-12 22:57:36 +0000

    Schedule

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,8 +4,32 @@ export default Ember.Route.extend({
   model: function() {
     return [
       {
-        title: "TBA",
-        date: "2019-12-12",
+        title: "Guest TBA",
+        date: "2020-01-09",
+        start: "14:00",
+        end: "16:00"
+      },
+      {
+        title: "Guest TBA",
+        date: "2020-02-06",
+        start: "14:00",
+        end: "16:00"
+      },
+      {
+        title: "Guest TBA",
+        date: "2020-03-05",
+        start: "14:00",
+        end: "16:00"
+      },
+      {
+        title: "Guest TBA",
+        date: "2020-04-02",
+        start: "14:00",
+        end: "16:00"
+      },
+      {
+        title: "Guest TBA",
+        date: "2020-04-30",
         start: "14:00",
         end: "16:00"
       }
