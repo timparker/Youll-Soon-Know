e5b460420d473af0da7244df2d505a61e80b0949
Tim Parker <tim@iamtimparker.com>
2020-10-15 18:36:12 +0100

    Redirect

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -5,12 +5,6 @@ export default Ember.Route.extend({
     return [
       {
         title: "Guest TBA",
-        date: "2020-10-15",
-        start: "14:00",
-        end: "16:00"
-      },
-      {
-        title: "Guest TBA",
         date: "2020-11-12",
         start: "14:00",
         end: "16:00"
