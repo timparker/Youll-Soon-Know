7443c8c8d653868c7403d07628c3095974470d40
Tim Parker <tim@iamtimparker.com>
2020-12-22 17:33:58 +0000

    Show dates 2021

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -5,7 +5,79 @@ export default Ember.Route.extend({
     return [
       {
         title: "TBD",
-        date: "2021-01-01",
+        date: "2021-01-07",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-02-04",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-03-04",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-04-01",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-04-29",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-05-27",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-06-24",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-07-22",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-08-19",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-09-16",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-10-14",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-11-11",
+        start: "15:00",
+        end: "16:00"
+      },
+      {
+        title: "TBD",
+        date: "2021-12-09",
         start: "15:00",
         end: "16:00"
       }
