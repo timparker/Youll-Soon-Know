a2055f996ec9da0efbb81e0a3760f838a9d0d86d
Tim Parker <timparker@tims-mbp.home>
2021-03-28 15:23:10 +0100

    10th anniversary

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -4,12 +4,6 @@ export default Ember.Route.extend({
   model: function() {
     return [
       {
-        title: "10th Anniversary w/S.Maharba",
-        date: "2021-04-01",
-        start: "15:00",
-        end: "16:00"
-      },
-      {
         title: "TBD",
         date: "2021-04-29",
         start: "15:00",
